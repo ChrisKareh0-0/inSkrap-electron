bca7958e2c257a1a20dd2439964a1cee91170b0f
Anthony Saliba <anthonysaliba93@gmail.com>
2024-09-11 23:44:26 +0300

    Fix landing page alignment

--- inskrap-frontend/src/renderer/App.jsx
+++ inskrap-frontend/src/renderer/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Route, Routes, Link, HashRouter } from 'react-router-dom';
+import React from "react";
+import { Route, Routes, Link, HashRouter } from "react-router-dom";
 
-import NeuralNoiseBackground from './Components/background';
-import SearchPage from './Pages/searchPage';
-import './App.css';
+import NeuralNoiseBackground from "./Components/background";
+import SearchPage from "./Pages/searchPage";
+import "./App.css";
 
 function App() {
-  console.log("is the project running")
+  console.log("is the project running");
   return (
     <HashRouter>
       <div className="App">
@@ -22,17 +22,21 @@ function App() {
 
 function Home() {
   return (
-    <div className="home">
+    <>
       <NeuralNoiseBackground />
-      <div className="content">
-        <h1 style={{color: '#fff'}}>Welcome to inSkrap</h1>
-        {/* <p style={{color:'#fff'}}>This is supposed to be the landing page.</p> */}
-        <Link to="/search">
-        <button className="btn"><i className="animation"></i>Get Started<i className="animation"></i>
-        </button>
-        </Link>
+      <div className="home">
+        <div className="content">
+          <h1>Welcome to inSkrap</h1>
+          {/* <p style={{color:'#fff'}}>This is supposed to be the landing page.</p> */}
+          <Link to="/search">
+            <button className="btn">
+              <i className="animation"></i>Get Started
+              <i className="animation"></i>
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
